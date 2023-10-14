14fe006bb186278c83e0bed288fe70273429177f
Ulises Capistran <ucapistranmtz@gmail.com>
2023-10-14 08:51:42 -0500

    lesson 351

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,12 +1,62 @@
-import React from 'react'
-import { Navbar } from '../components/Navbar'
 
+
+
+import React from 'react'
+import enUS from 'date-fns/locale/en-US'
+import { Navbar } from '../'
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import { addHours, format, parse, startOfWeek, getDay } from 'date-fns'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import { getMessagesES, localizer } from '../../helpers'
+const locales = {
+  'en-US': enUS,
+}
+
+
+
+const events = [{
+
+  title: 'birthday',
+  start: new Date(),
+  end: addHours(new Date(), 2),
+  bgColor: '#fafa',
+  user: {
+    _id: '1231',
+    name: 'Ulises'
+  }
+}]
 export const CalendarPage = () => {
+
+  const eventStyleGetter = (event, stsart, end, isSelected) => {
+
+    console.log({ event, stsart, end, isSelected });
+
+    const style = {
+      backgroundColir: '#347CF7',
+      borderRadious:'0px',
+      opacity:0.8,
+      color:'white'
+    }
+    return {
+      style
+    }
+
+  }
   return (
-     <>
-     <Navbar>
-      
-     </Navbar>
-     </>
+    <>
+
+      <Navbar />
+      <Calendar
+        culture='es'
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc(100vh - 80px)' }}
+        messages={getMessagesES()}
+        eventPropGetter={eventStyleGetter}
+
+      />
+    </>
   )
 }
