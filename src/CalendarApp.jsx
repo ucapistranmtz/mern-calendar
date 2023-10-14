c814c1f2d7936c33d2d33bbc38bcee05f37a7edb
Ulises Capistran <ucapistranmtz@gmail.com>
2023-10-14 06:28:00 -0500

    lesson 347

---
+++ src/CalendarApp.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const CalendarApp = () => {
+  return (
+    <h1>CalendarApp</h1>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+ 
+import './styles.css'
+import { CalendarApp } from './CalendarApp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <CalendarApp />
   </React.StrictMode>,
 )
