546d12ad9c9037f077fe39e376abf9b955151d8b
Ulises Capistran <ucapistranmtz@gmail.com>
2023-10-14 08:06:54 -0500

    lesson 349

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Navbar } from '../components/Navbar'
 
 export const CalendarPage = () => {
   return (
-    <div>CalendarPage</div>
+     <>
+     <Navbar>
+      
+     </Navbar>
+     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
- 
-import './styles.css'
-import { CalendarApp } from './CalendarApp'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { CalendarApp } from './CalendarApp';
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <CalendarApp />
-  </React.StrictMode>,
+  // </React.StrictMode>
 )
